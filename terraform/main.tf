5c4681845cddb943a43b4c6ba523a630696e29ad
Ofer Mironi <oferm@cloudvalley.io>
2021-10-17 12:22:34 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,10 +117,19 @@ resource "azurerm_container_group" "jmeter_workers" {
   }
 }
 
+resource "null_resource" "previous" {}
 
 resource "time_sleep" "wait_120_seconds" {
+  depends_on = [null_resource.previous]
+
   create_duration = "120s"
 }
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_120_seconds]
+}
+ 
 
 resource "azurerm_container_group" "jmeter_controller" {
   name                = "${var.PREFIX}-controller"
