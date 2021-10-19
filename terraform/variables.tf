ff470d139f80ebb9c19fdebc5c847ae9e034092a
Ofer Mironi <oferm@cloudvalley.io>
2021-10-19 13:23:44 +0000

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "RESOURCE_GROUP_NAME" {
   type    = string
-  default = "jmeter2"
+  default = "jmeter3"
 }
 
 
@@ -16,7 +16,7 @@ variable "LOCATION" {
 
 variable "PREFIX" {
   type    = string
-  default = "jmeter2"
+  default = "jmeter3"
 }
 
 variable "VNET_ADDRESS_SPACE" {
