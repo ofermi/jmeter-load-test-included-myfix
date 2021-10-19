eb5b37efbfa7fb83a6dcf9535181adbbcca47031
Ofer Mironi <oferm@cloudvalley.io>
2021-10-19 03:51:39 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ data  "azurerm_subnet" "jmeter_subnet" {
 
 
 data   "azurerm_storage_account" "jmeter_storage" {
-  resource_group_name  = "jmeter"                 = "jmeter"
+  resource_group_name  = "jmeter"                 
   name = "jmeterstoraged9541e99"
   location            = "westeurope"
 }
