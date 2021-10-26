08761702ac424b87b553d30ff0cf9a82e9f7190b
Ofer Mironi <oferm@cloudvalley.io>
2021-10-26 15:51:22 +0000

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "RESOURCE_GROUP_NAME" {
   type    = string
-  default = "jmete20"
+  default = "jmete21"
 }
 
 
@@ -21,7 +21,7 @@ variable "LOCATION" {
 
 variable "PREFIX" {
   type    = string
-  default = "jmete20"
+  default = "jmete21"
 }
 
 variable "VNET_ADDRESS_SPACE" {
